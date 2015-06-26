60c4f87d17712b0df411ffa5cc87b2f0d8099e25
Arifin Luthfi P <arifin.luthfi@gmail.com>
2015-06-26 15:09:43 +0700

    Setup project, ignore storyboard

--- PhotoGaler/AppDelegate.m
+++ PhotoGaler/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    // Create root view controller
+    ViewController* intro = [[ViewController alloc] init];
+    [self.window setRootViewController:intro];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
